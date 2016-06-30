50f0d25ed101f366b7fc55f6de9ccb7568d2a84d
M!nified <gora.pub@gmail.com>
2016-06-30 13:29:24 +0200

    +menu

--- templates/header.php
+++ templates/header.php
@@ -1,6 +1,16 @@
 <header class="banner">
   <div class="container">
     <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
+    <nav class="nav-top-bar">
+      <?php
+      if(has_nav_menu('top_navigation')) :
+        wp_nav_menu([
+          'theme_location' => 'top_navigation',
+          'menu_class' => 'menu-bar'
+        ]);
+      endif;
+      ?>
+    </nav>
     <nav class="nav-primary">
       <?php
       if (has_nav_menu('primary_navigation')) :
