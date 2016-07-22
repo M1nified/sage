e5a30226aebdd33dcc56204cb7a3a19407de8e46
M!nified <gora.pub@gmail.com>
2016-07-22 12:33:27 +0200

    post anchor (post_[ID])

--- templates/content.php
+++ templates/content.php
@@ -1,5 +1,6 @@
 <article <?php post_class(); ?>>
   <header>
+    <a name="post_<?php echo get_the_ID(); ?>"></a>
     <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
     <?php get_template_part('templates/entry-meta'); ?>
   </header>
