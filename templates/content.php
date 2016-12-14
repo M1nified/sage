88b5d14ba0b61b4fc4d61ae85bf7b0ab2534939b
M!nified <gora.pub@gmail.com>
2016-12-14 09:12:05 +0100

    hide post author and date on the list

--- templates/content.php
+++ templates/content.php
@@ -1,8 +1,10 @@
 <article <?php post_class(); ?>>
   <header>
     <a name="post_<?php echo get_the_ID(); ?>"></a>
-    <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-    <?php get_template_part('templates/entry-meta'); ?>
+    <h2 class="entry-title"><?php /*<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+    <?php get_template_part('templates/entry-meta'); ?>*/
+    the_title();
+    ?>
   </header>
   <div class="entry-summary">
     <?php the_excerpt(); ?>
