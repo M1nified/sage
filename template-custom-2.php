d4099057761d405371c622f70a3e689696a41e40
M!nified <gora.pub@gmail.com>
2016-12-15 12:22:25 +0100

    +.theloop-wrapper and article-separator bordered if :not(:last-child)

--- template-custom-2.php
+++ template-custom-2.php
@@ -3,6 +3,8 @@
  * Template Name: No Title
  */
  ?>
+<div class="theloop-wrapper">
 <?php while (have_posts()) : the_post(); ?>
   <?php get_template_part('templates/content', 'page'); ?>
 <?php endwhile; ?>
+</div>
